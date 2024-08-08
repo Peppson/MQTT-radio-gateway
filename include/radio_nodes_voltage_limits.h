ebbd8ee8afab23a657daaa7828ef88160113b114
Jesper Wallentin <132165471+Peppson@users.noreply.github.com>
2024-08-08 16:30:58 +0200

    Update radio_nodes_voltage_limits.h

--- include/radio_nodes_voltage_limits.h
+++ include/radio_nodes_voltage_limits.h
@@ -6,9 +6,7 @@
 
 // node_ID, volt_max, volt_min
 constexpr const uint16_t RADIO_NODES_VOLT_LIMITS[][3] = {
-    {1, 973, 766},
-    {2, 222, 222},
-    {3, 333, 333},
-    {4, 444, 444},
-    {255, 999, 999},
+    {1, 899, 730},
+    {2, 900, 725},
+    {255, 999, 999} // Debug
 };
